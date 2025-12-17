87efaf2cb9a1d103ceb53be07957ca36ffd94393
Matiaz-hp <huanuirmatiaz@gmail.com>
2025-12-17 10:10:42 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,19 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCgVrsHW7MzvwKBiIXXqt2zHeI9NpJ7pp8",
   authDomain: "nauta-tours.firebaseapp.com",
   projectId: "nauta-tours",
   storageBucket: "nauta-tours.firebasestorage.app",
   messagingSenderId: "896950306295",
-  appId: "1:896950306295:web:59e2a677fc9efab6b69af2",
-  measurementId: "G-743FVWPD4H"
+  appId: "1:896950306295:web:59e2a677fc9efab6b69af2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
