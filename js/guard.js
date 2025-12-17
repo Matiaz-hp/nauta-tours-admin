c9bcb48a27f56070245cfc156666e249f44ace60
Matiaz-hp <huanuirmatiaz@gmail.com>
2025-12-17 11:43:35 -0500

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,31 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+// 🔐 Guard del panel admin (GitHub Pages)
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCgVrsHW7MzvwKBiIXXqt2zHeI9NpJ7pp8",
-  authDomain: "nauta-tours.firebaseapp.com",
-  projectId: "nauta-tours"
-};
+const isAdmin = localStorage.getItem("adminAuth");
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// 🔐 LISTA BLANCA DE ADMINS
-const ADMINS = [
-  "admin@nautatours.com",
-  "admin123@gmail.com"
-];
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "index.html";
-    return;
-  }
-
-  const email = user.email.toLowerCase();
-
-  if (!ADMINS.includes(email)) {
-    alert("Acceso no autorizado");
-    window.location.href = "index.html";
-  }
-});
+if (isAdmin !== "true") {
+  alert("Acceso no autorizado");
+  window.location.href = "index.html";
+}
