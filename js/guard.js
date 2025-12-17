c312d14632959860715180c34201288a5c64887e
Matiaz-hp <huanuirmatiaz@gmail.com>
2025-12-17 11:13:45 -0500

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,1 +1,29 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgVrsHW7MzvwKBiIXXqt2zHeI9NpJ7pp8",
+  authDomain: "nauta-tours.firebaseapp.com",
+  projectId: "nauta-tours"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const ref = doc(db, "users", user.uid);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists() || snap.data().role !== "admin") {
+    alert("Acceso no autorizado");
+    window.location.href = "index.html";
+  }
+});
