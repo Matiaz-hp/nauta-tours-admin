19227e69e43b72de181d07c330def2238af60089
Matiaz-hp <huanuirmatiaz@gmail.com>
2025-12-17 11:39:15 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -10,15 +10,29 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+// 👉 correos admin permitidos
+const ADMINS = [
+  "admin@nautatours.com",
+  "admin123@gmail.com"
+];
+
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const email = e.target[0].value;
+  const email = e.target[0].value.trim().toLowerCase();
   const password = e.target[1].value;
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    window.location.href = "dashboard.html";
+
+    // 🔐 marcar sesión admin
+    if (ADMINS.includes(email)) {
+      localStorage.setItem("adminAuth", "true");
+      window.location.href = "dashboard.html";
+    } else {
+      alert("Acceso no autorizado");
+    }
+
   } catch (error) {
     alert("Correo o contraseña incorrectos");
   }
