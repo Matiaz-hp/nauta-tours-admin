6e77497ecb3a60cad1e9e382ad23288c3ab9685f
Matiaz-hp <huanuirmatiaz@gmail.com>
2025-12-17 11:33:17 -0500

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,7 +1,5 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgVrsHW7MzvwKBiIXXqt2zHeI9NpJ7pp8",
@@ -11,18 +9,22 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-onAuthStateChanged(auth, async (user) => {
+// 🔐 LISTA BLANCA DE ADMINS
+const ADMINS = [
+  "admin@nautatours.com",
+  "admin123@gmail.com"
+];
+
+onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "index.html";
     return;
   }
 
-  const ref = doc(db, "users", user.uid);
-  const snap = await getDoc(ref);
+  const email = user.email.toLowerCase();
 
-  if (!snap.exists() || snap.data().role !== "admin") {
+  if (!ADMINS.includes(email)) {
     alert("Acceso no autorizado");
     window.location.href = "index.html";
   }
